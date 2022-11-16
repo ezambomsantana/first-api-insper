7f3a36fd4c862272f25f2cb60976073a13e8b69f
Eduardo Santana <ezambomsantana@gmail.com>
2022-11-16 16:59:42 -0300

    first

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+
+    @InjectMocks
+    private UserController userController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    public void testListUsers() throws Exception {
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.get("/user"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String resp = result.getResponse().getContentAsString();
+        Assertions.assertEquals("[{\"nome\":\"Nome 1\"," +
+                        "\"cpf\":\"123\",\"endereco\":\"endereco\",\"key\":null," +
+                        "\"email\":null,\"telefone\":\"5432\",\"dataCadastro\":null}]"
+                , resp);
+    }
+
+}
